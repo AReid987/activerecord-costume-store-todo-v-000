37ee980cce5b8fdd8a835e86a304d1f8844c268a
Antonio Reid <read.musik@gmail.com>
2018-03-28 00:43:49 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,10 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.float "price"
-    t.string "url"
+    t.string "image_url"
     t.string "size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
@@ -37,6 +39,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
+    t.string "description"
   end
 
 end
