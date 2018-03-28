0315e15dc76de23811073bba1b3c738a17cd4a9c
Antonio Reid <read.musik@gmail.com>
2018-03-28 00:23:09 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
 
   def change
     create_table :costumes do |t|
-      t.string :name
+      t.name :string 
       t.price :float
       t.url :string
       t.size :string
